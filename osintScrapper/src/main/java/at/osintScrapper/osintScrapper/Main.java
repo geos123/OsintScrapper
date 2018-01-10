67226e995689abd50374d309cb4e6a3be92495e8
geos <geraldoswald1@gmx.at>
2018-01-10 21:50:29 +0100

    Initial Commit

---
+++ osintScrapper/src/main/java/at/osintScrapper/osintScrapper/Main.java
@@ -0,0 +1,16 @@
+package at.osintScrapper.osintScrapper;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/**
+ * OsintScrapper Main
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ChromeOptions chromeOptions = new ChromeOptions();
+    }
+}
